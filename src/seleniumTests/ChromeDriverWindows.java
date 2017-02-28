2defcc89b6db1b84b8258242c0db3866e3a2dbba
George Gutierrez <gmgutierrez87@gmail.com>
2017-02-27 23:27:47 -0800

    replace JUnit class with TestNG class

--- src/seleniumTests/ChromeDriverWindows.java
+++ src/seleniumTests/ChromeDriverWindows.java
@@ -1,20 +1,49 @@
 package seleniumTests;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
 public class ChromeDriverWindows {
-
-	public static void main(String[] args) {
-		String baseURL = "http://letskodeit.teachable.com/p/practice";
+	WebDriver driver;
+	String baseUrl;
+	static Logger log = Logger.getLogger(ChromeDriverWindows.class);
+	
+	@BeforeClass
+	public void setUp() throws Exception{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\George\\workspace\\selenium-demo\\libs\\chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "https://demostore.x-cart.com/";
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test(groups = {"search", "checkout"})
+	public void search() {
+		driver.get(baseUrl);
+		driver.findElement(By.xpath("//div[@id='search']//input[@id='substring-default']")).sendKeys("Iron Man");
+		driver.findElement(By.xpath("//div[@id='search']//input[@id='substring-default']//parent::span//parent::div/following-sibling::button")).click();
+		WebElement productPhoto = driver.findElement(By.xpath("//ul[@class='products-grid grid-list']//div[@class='product-photo']"));
+		Actions action = new Actions(driver);
+		// move mouse cursor over photo to bring up pop-up menu
+		action.moveToElement(productPhoto).perform();
+	}
+	
+	@Test
+	public void checkout(){
 		
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get(baseURL);
-		System.out.println(driver.findElement(By.xpath(".//*[@id='product']/tbody/tr[3]/td[2]/following-sibling::td")).getText());
+	}
+	@AfterClass
+	public void tearDown() throws Exception{
+		driver.quit();
 	}
 
 }
-
